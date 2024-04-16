4f81b00de7bbd6cec4cf6230bb82c2e0a5fd0ab9
Vihaan Sondhi <78501480+Vihaan3@users.noreply.github.com>
2024-04-16 12:11:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
     >
             <strong>Welcome to Acme.</strong> This is a modified example of the{' '}  
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+               Next.js Learn Course
             </a>
             created by Vercel made by HackTTC to learn Next.js.
           </p>
